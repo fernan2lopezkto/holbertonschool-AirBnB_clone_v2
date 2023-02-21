f1ca51fa53f5b986b086ea12217cf4b1439d60e0
juan latitude <fernan2lopezkto@gmail.com>
2023-02-21 12:27:42 -0300

    try to check

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
-''' 2-c_route module '''
-
-
-from flask import Flask
-
+""" Write a script that starts a Flask web application """
 
 from markupsafe import escape
+
+from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def root_hello():
-    '''this function return a simple nessage in root path'''
-    return 'Hello HBNB!'
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb_hello():
-    '''this function return a simple nessage in /hbnb path'''
-    return 'HBNB'
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    '''this function return a string whith underscore symbol remplaced as space'''
-    new_text = text.remplace('_', ' ')
-    return f"C + {escape(new_text)}"
+@app.route("/c/<text>", strict_slashes=False)
+def cmessage(text):
+    return 'C {}'.format(escape(text.replace("_", " ")))
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
