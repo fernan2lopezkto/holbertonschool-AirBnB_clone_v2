fde995f9a95c741ff9cd63fbcc8272e692e33f8f
juan latitude <fernan2lopezkto@gmail.com>
2023-02-21 11:46:10 -0300

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+''' 1-hbnb_route.py module '''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def root_hello():
+    '''this function return a simple nessage in root path'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_hello():
+    '''this function return a simple nessage in /hbnb path'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    '''this function return a string whith underscore symbol remplaced as space'''
+    return text.remplace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
