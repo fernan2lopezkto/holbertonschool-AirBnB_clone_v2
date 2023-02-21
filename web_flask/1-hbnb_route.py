2ef9af440e5baff6c59e5a21f524dd10fab61994
juan latitude <fernan2lopezkto@gmail.com>
2023-02-21 11:26:37 -0300

    documentation and fix pep 8

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,10 +8,13 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def root_hello():
+    '''this function return a simple nessage in root path'''
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb_hello():
+    '''this function return a simple nessage in /hbnb path'''
     return 'HBNB'
 
 if __name__ == '__main__':
